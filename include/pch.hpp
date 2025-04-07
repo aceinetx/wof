b708d0a8a9ef9be4d63aab1329fbcd1bbe11f987
aceinetx <mouselin82@gmail.com>
2025-04-07 14:36:56 +0300

    Initial commit

---
+++ include/pch.hpp
@@ -0,0 +1,16 @@
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/Support/raw_ostream.h>
+#include <map>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
---
+++ src/wofc.cc
@@ -0,0 +1,5 @@
+#include <pch.hpp>
+
+int main() {
+	std::cout << "wofc\n";
+}
